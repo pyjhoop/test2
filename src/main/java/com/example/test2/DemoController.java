1f4a2d507f6d53a31ecbd65491135d4d1640e825
pyjhoop <pyjhoop@naver.com>
2023-12-03 14:48:08 +0900

    application.yml 값 변경

--- src/main/java/com/example/test2/DemoController.java
+++ src/main/java/com/example/test2/DemoController.java
@@ -1,13 +1,17 @@
 package com.example.test2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${override.value}")
+    private String value;
+
     @GetMapping("/api/test")
     public String test(){
-        return "test";
+        return value;
     }
 }
